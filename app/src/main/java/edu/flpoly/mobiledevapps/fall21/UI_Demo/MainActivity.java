38b42d7bd6d4bba7a8793575ec745ab4de001a99
AnrdyShmrdy <andy.ponce@outlook.com>
2021-10-14 11:57:56 -0400

    Added Spinners to activities. Fixed view and widget constraints

--- app/src/main/java/edu/flpoly/mobiledevapps/fall21/UI_Demo/MainActivity.java
+++ app/src/main/java/edu/flpoly/mobiledevapps/fall21/UI_Demo/MainActivity.java
@@ -2,13 +2,45 @@ package edu.flpoly.mobiledevapps.fall21.UI_Demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.app_screen_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.app_screens, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        // TODO Auto-generated method stub
     }
 }
